ce31b61389fbd581eddb2fcc2cd64c4b362ec546
Will Goldie <wgoldie@gmail.com>
2018-11-01 12:26:51 -0700

    Updates

---
+++ precache-manifest.ee6db7c8c4db98fd740e5c457eea8df0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7115003af526270aaa32",
+    "url": "/react-cytoscape-tools-demos/static/js/runtime~main.7115003a.js"
+  },
+  {
+    "revision": "160e721c291f5cb1a3d3",
+    "url": "/react-cytoscape-tools-demos/static/js/main.160e721c.chunk.js"
+  },
+  {
+    "revision": "c9623a8219b100a1b273",
+    "url": "/react-cytoscape-tools-demos/static/js/1.c9623a82.chunk.js"
+  },
+  {
+    "revision": "160e721c291f5cb1a3d3",
+    "url": "/react-cytoscape-tools-demos/static/css/main.0f6c7a5f.chunk.css"
+  },
+  {
+    "revision": "e636889170a146b2bf6b3c305344fe23",
+    "url": "/react-cytoscape-tools-demos/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-cytoscape-tools-demos/precache-manifest.ee6db7c8c4db98fd740e5c457eea8df0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-cytoscape-tools-demos/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
